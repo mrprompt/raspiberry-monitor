c46df25c5e22ca6860f669fe8313388040231481
Thiago Paes <mrprompt@gmail.com>
2018-09-12 14:09:28 -0300

    add thingspeak

--- monitor.py
+++ monitor.py
@@ -1,5 +1,11 @@
 from rpyc.utils.zerodeploy import MultiServerDeployment
 from plumbum import SshMachine
+from dotenv import load_dotenv
+import time
+import os
+import thingspeak
+
+load_dotenv()
 
 def check_process(conn, process):
     proc = conn.modules.subprocess.Popen(["pidof", process], stdout = -1, stderr = -1)
@@ -7,17 +13,39 @@ def check_process(conn, process):
 
     return len(stdout.split()) > 0
 
-servers = [
-    SshMachine("192.168.11.26"), # meteormon2 
-    SshMachine("192.168.11.32"), # vpn
-    SshMachine("192.168.11.35"), # meteormon1
-]
+channel = thingspeak.Channel(os.environ['THINGSPEAK_CHANNEL'])
 
-deployment = MultiServerDeployment(servers)
-conn1, conn2, conn3 = deployment.classic_connect_all()
+while True:
+    servers = [
+        SshMachine("192.168.11.26"), # meteormon2 
+        SshMachine("192.168.11.32"), # vpn
+        SshMachine("192.168.11.35"), # meteormon1
+    ]
 
-print("Camera #1 running: ", check_process(conn3, "meteotux_pi"))
-print("Camera #2 running: ", check_process(conn1, "meteotux_pi"))
-print("VPN running: ", check_process(conn2, "openvpn"))
+    deployment = MultiServerDeployment(servers)
+    conn1, conn2, conn3 = deployment.classic_connect_all()
 
-deployment.close()
+    check_1 = check_process(conn3, "meteotux_pi")
+    check_2 = check_process(conn1, "meteotux_pi")
+    check_3 = check_process(conn2, "openvpn")
+
+    print("Camera #1 running: ", check_1)
+    print("Camera #2 running: ", check_2)
+    print("VPN running: ", check_3)
+
+    deployment.close()
+
+    params = {
+        'api_key': os.environ['THINGSPEAK_KEY'],
+        'field1': int(check_1),
+        'field2': int(check_2),
+        'field3': int(check_3)
+    }
+
+    try:
+        channel.update(params)
+    except (Exception):
+        print("Olha a excessao", Exception)
+        pass
+
+    time.sleep(3600)
